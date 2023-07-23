e6a043b2739f5284ac9561dddb0e27cd906384fa
Max Ryndin <rgmasim@gmail.com>
2023-07-24 00:29:32 +0800

    Cleanup app e2e test.

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,18 +1,22 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
+
 import { AppModule } from './../src/app.module';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
+  let module: TestingModule;
 
-  beforeEach(async () => {
-    const moduleFixture: TestingModule = await Test.createTestingModule({
-      imports: [AppModule],
-    }).compile();
+  beforeAll(async () => {
+    module = await Test.createTestingModule({ imports: [AppModule] }).compile();
 
-    app = moduleFixture.createNestApplication();
+    app = module.createNestApplication();
     await app.init();
+  });
+
+  afterAll(async () => {
+    await app.close();
   });
 
   it('/ (GET)', () => {
@@ -20,44 +24,5 @@ describe('AppController (e2e)', () => {
       .get('/')
       .expect(200)
       .expect('Hello World!');
-  });
-});
-
-describe('F1S1 Login flow', () => {
-  let app: INestApplication;
-
-  beforeEach(async () => {
-    const moduleFixture: TestingModule = await Test.createTestingModule({
-      imports: [AppModule],
-    }).compile();
-
-    app = moduleFixture.createNestApplication();
-    await app.init();
-  });
-
-  describe('endpoint under JwtAuthGuard (eg, /profile)', () => {
-    let access_token: string;
-
-    it('should return HTTP 401 if no auth', () => {
-      return request(app.getHttpServer()).get('/profile').expect(401);
-    });
-
-    it('should return access token for valid creds', async () => {
-      const res = await request(app.getHttpServer())
-        .post('/auth/login')
-        .send({ username: 'john@test.com', password: 'xx' })
-        .set('Content-Type', 'application/json')
-        .expect(201);
-
-      access_token = res.body?.access_token;
-      expect(access_token).toBeTruthy();
-    });
-
-    it('should return HTTP 200 for valid access token', () => {
-      return request(app.getHttpServer())
-        .get('/profile')
-        .set('Authorization', `Bearer ${access_token}`)
-        .expect(200);
-    });
   });
 });
