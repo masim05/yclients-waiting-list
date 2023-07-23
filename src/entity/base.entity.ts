0fe43596b569dbc6c3f0fd5e18e6ad5f53fce3da
Max Ryndin <rgmasim@gmail.com>
2023-07-23 08:20:46 +0800

    Format code.

--- src/entity/base.entity.ts
+++ src/entity/base.entity.ts
@@ -8,9 +8,17 @@ export abstract class BaseEntity {
   @PrimaryGeneratedColumn('uuid')
   uuid: string;
 
-  @CreateDateColumn({ name: 'created_at', type: 'timestamptz', default: () => 'CURRENT_TIMESTAMP' })
+  @CreateDateColumn({
+    name: 'created_at',
+    type: 'timestamptz',
+    default: () => 'CURRENT_TIMESTAMP',
+  })
   createdAt: Date;
 
-  @UpdateDateColumn({ name: 'updated_at', type: 'timestamptz', default: () => 'CURRENT_TIMESTAMP' })
+  @UpdateDateColumn({
+    name: 'updated_at',
+    type: 'timestamptz',
+    default: () => 'CURRENT_TIMESTAMP',
+  })
   updatedAt: Date;
 }
